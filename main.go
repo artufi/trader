b70cc585f3eef1d7f23ae3c2a4841974f4580942
artufi <aartur.fil@gmail.com>
2024-02-18 16:04:48 +0100

    Initialize main logic

--- main.go
+++ main.go
@@ -1,14 +1,32 @@
 package main
 
-type Config struct {
-	XTB struct {
-		AuthURL string `yaml:"authURL"`
-	} `yaml:"XTB"`
-}
+import (
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	ws "github.com/gorilla/websocket"
+	"log/slog"
+	"net/http"
+	"path/filepath"
+	"trader/config"
+	"trader/controller"
+	"trader/infrastructure/websocket"
+	"trader/logging"
+)
+
+var env = "dev"
 
 func main() {
-	//plik z logami
-	//logger inicjalizacja
-	//wczytanie konfiguracji
-	//uruchomienie serwera HTTP nasluchujacego na requesty z innego serwisu
+	logFile := logging.Must(logging.GetLogFile("app.log"))
+	logger := slog.New(slog.NewJSONHandler(logFile, nil))
+
+	cfg := config.Config{}
+	config.Must(config.LoadYAMLConf(filepath.Join("config", fmt.Sprintf("cfg-%s.yaml", env)), &cfg))
+
+	dialer := &ws.Dialer{}
+	wsManager := websocket.NewWSManager(dialer, logger)
+
+	r := chi.NewRouter()
+	r.Get("/payload", controller.TradeHandler(cfg, wsManager, logger))
+
+	http.ListenAndServe(":3000", r)
 }
