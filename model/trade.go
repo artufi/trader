e507856bf544f98d877143c485dfbdb4d8d90061
artufi <aartur.fil@gmail.com>
2024-02-18 15:55:48 +0100

    Add a special structure to describe the main payload

---
+++ model/trade.go
@@ -0,0 +1,14 @@
+package model
+
+type TradeInstruction struct {
+	Symbol     string  `json:"symbol"`
+	Date       string  `json:"date"`
+	Allocation float64 `json:"allocation"`
+	PredsProba float64 `json:"preds_proba"`
+	StopLoss   float64 `json:"stop_loss"`
+	ModelSetup struct {
+		ForecastHorizon int    `json:"forecast_horizon"`
+		TargetChange    int    `json:"target_change"`
+		ModelType       string `json:"model_type"`
+	} `json:"model_setup"`
+}
