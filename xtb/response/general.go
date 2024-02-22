88d342b8ccb2086f75be4967684b1f72a451b3df
artufi <aartur.fil@gmail.com>
2024-02-22 08:59:18 +0100

    Add field to GeneralResponse

--- xtb/response/general.go
+++ xtb/response/general.go
@@ -1,8 +1,9 @@
 package response
 
 type GeneralResponse struct {
-	Status     bool                   `json:"status"`
-	ReturnData map[string]interface{} `json:"returnData,omitempty"`
-	ErrorCode  string                 `json:"errorCode,omitempty"`
-	ErrorDescr string                 `json:"errorDescr,omitempty"`
+	Status          bool                   `json:"status"`
+	StreamSessionId string                 `json:"streamSessionId"`
+	ReturnData      map[string]interface{} `json:"returnData,omitempty"`
+	ErrorCode       string                 `json:"errorCode,omitempty"`
+	ErrorDescr      string                 `json:"errorDescr,omitempty"`
 }
