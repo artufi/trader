d33c1b095326f4234c863d33137c5e89131be2a2
artufi <aartur.fil@gmail.com>
2024-02-27 08:34:32 +0100

    Add function to load FS files

--- config/yaml.go
+++ config/yaml.go
@@ -3,6 +3,7 @@ package config
 import (
 	"fmt"
 	"gopkg.in/yaml.v3"
+	"io/fs"
 	"os"
 )
 
@@ -16,6 +17,18 @@ func Must(err error) {
 // conf must be pointer
 func LoadYAMLConf(name string, conf interface{}) error {
 	data, err := os.ReadFile(name)
+	if err != nil {
+		return fmt.Errorf("failed to read file=%q: %w", name, err)
+	}
+	err = yaml.Unmarshal(data, conf)
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal YAML file=%q: %w", name, err)
+	}
+	return nil
+}
+
+func LoadFSYAMLConf(fsys fs.FS, name string, conf interface{}) error {
+	data, err := fs.ReadFile(fsys, name)
 	if err != nil {
 		return fmt.Errorf("failed to read file=%q: %w", name, err)
 	}
