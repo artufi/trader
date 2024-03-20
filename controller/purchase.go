073c57921a9e79a1c784b282f5ab692e97857b63
artufi <aartur.fil@gmail.com>
2024-03-20 15:01:43 +0100

    Define Purchase struct to manage and group Purchase related functions

--- controller/purchase.go
+++ controller/purchase.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/artufi/trader/config"
 	"github.com/artufi/trader/controller/middleware"
@@ -9,182 +10,239 @@ import (
 	"github.com/artufi/trader/logging"
 	"github.com/artufi/trader/model"
 	"github.com/artufi/trader/xtb/processor"
+	"github.com/artufi/trader/xtb/response"
 	"log/slog"
 	"net/http"
 )
 
-func PurchasesHandler(cfg config.AppConfig, wsManager *websocket.WSManager, logger *slog.Logger) http.HandlerFunc {
+type Purchase struct {
+	Cfg       config.AppConfig
+	WSManager *websocket.WSManager
+	Logger    *slog.Logger
+
+	PositionService model.PositionService
+	OrderService    model.OrderService
+}
+
+func (p *Purchase) PurchasesHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
-		logger.InfoContext(ctx, "Start processing purchases request")
+		p.Logger.InfoContext(ctx, "Start processing purchases request")
 
 		connDetails, ok := ctx.Value(middleware.ConnDetailsKey).(middleware.ConnDetails)
 		if !ok {
-			logger.ErrorContext(ctx, "Failed to get ConnDetails")
+			p.Logger.ErrorContext(ctx, "Failed to get ConnDetails")
 			http.Error(w, "Could not retrieve client details", http.StatusInternalServerError)
 			return
 		}
 		traceID := connDetails.TraceID.String()
 
-		wsClient, err := wsManager.DialForNewClient(ctx, cfg.XTB.Demo.WebSocketURL, nil)
+		wsClient, err := p.WSManager.DialForNewClient(ctx, p.Cfg.XTB.Demo.WebSocketURL, nil)
 		if err != nil {
-			logger.ErrorContext(ctx, "Failed to create a new client", logging.ErrorAttr(err))
+			p.Logger.ErrorContext(ctx, "Failed to create a new client", logging.ErrorAttr(err))
 			http.Error(w, "Failed to establish client connection", http.StatusInternalServerError)
 			return
 		}
 		defer func() {
-			wsManager.RemoveClient(ctx, wsClient)
+			p.WSManager.RemoveClient(ctx, wsClient)
 		}()
 
-		h := Handler{
+		// init api handler
+		apiH := APIHandler{
 			Proc:    processor.NewProc(wsClient),
 			TraceID: traceID,
 		}
 
+		dbH := DBHandler{
+			TraceID:         traceID,
+			Logger:          p.Logger,
+			PositionService: p.PositionService,
+			OrderService:    p.OrderService,
+		}
+
 		// log user into XTB
-		loginResponse, err := h.Login(ctx, cfg.XTB.Demo.UserID, cfg.XTB.Demo.Password)
+		loginResponse, err := apiH.Login(ctx, p.Cfg.XTB.Demo.UserID, p.Cfg.XTB.Demo.Password)
 		if err != nil {
-			logger.ErrorContext(ctx, "Failed to login", logging.ErrorAttr(err))
+			p.Logger.ErrorContext(ctx, "Failed to login", logging.ErrorAttr(err))
 			http.Error(w, "Failed to login", http.StatusBadRequest)
 			return
 		}
-		logger.InfoContext(ctx, "Successfully processed Login", logging.RespAttr(loginResponse))
+		p.Logger.InfoContext(ctx, "Successfully processed Login", logging.RespAttr(loginResponse))
 
 		// get purchase instructions
 		purchaseInstructions := make([]model.PurchaseInstruction, 0)
 		err = json.NewDecoder(r.Body).Decode(&purchaseInstructions)
 		if err != nil {
-			logger.ErrorContext(ctx, "Failed to read request body with purchase instruction", logging.ErrorAttr(err))
+			p.Logger.ErrorContext(ctx, "Failed to read request body with purchase instruction", logging.ErrorAttr(err))
 			http.Error(w, "Failed to read body", http.StatusBadRequest)
 			return
 		}
 
 		for _, purchaseInstruction := range purchaseInstructions {
-			logger.InfoContext(ctx, "Will process purchase instruction", logging.PurchaseInstrAttr(purchaseInstruction))
+			p.Logger.InfoContext(ctx, "Will process purchase instruction", logging.PurchaseInstrAttr(purchaseInstruction))
 
 			symbol := purchaseInstruction.Symbol
 
 			// get symbol details
-			symbolResponse, err := h.GetSymbolExtended(ctx, symbol)
+			symbolResponse, err := apiH.GetSymbolExtended(ctx, symbol)
 			if err != nil {
-				logger.ErrorContext(ctx, "Failed to process GetSymbol",
+				p.Logger.ErrorContext(ctx, "Failed to process GetSymbol",
 					logging.ErrorAttr(err),
 					logging.SymbolAttr(symbol))
 				continue
 			}
-			logger.InfoContext(ctx, "Successfully processed GetSymbol", logging.RespAttr(symbolResponse))
+			p.Logger.InfoContext(ctx, "Successfully processed GetSymbol", logging.RespAttr(symbolResponse))
 
 			// prepare TradeTransactionInfo to pass it to TradeTransaction as argument
 			tradeTransInfo, err := purchaseInstruction.PrepareTradeTransInfo(symbolResponse, 0.01)
 			if err != nil {
-				logger.WarnContext(ctx, "Failed to prepare TradeTransInfo", logging.ErrorAttr(err),
+				p.Logger.WarnContext(ctx, "Failed to prepare TradeTransInfo", logging.ErrorAttr(err),
 					logging.SymbolAttr(symbol))
 				continue
 			}
 
 			// process TradeTransaction
-			tradeResponse, err := h.TradeTransaction(ctx, symbol, tradeTransInfo)
+			tradeResponse, err := apiH.TradeTransaction(ctx, symbol, tradeTransInfo)
 			if err != nil {
-				logger.ErrorContext(ctx, "Failed to process TradeTransaction",
+				p.Logger.ErrorContext(ctx, "Failed to process TradeTransaction",
 					logging.ErrorAttr(err),
 					logging.SymbolAttr(symbol))
 				continue
 			}
-			logger.InfoContext(ctx, "Successfully processed TradeTransaction", logging.RespAttr(tradeResponse))
+			p.Logger.InfoContext(ctx, "Successfully processed TradeTransaction", logging.RespAttr(tradeResponse))
 
 			// process TradeTransactionStatus
-			tradeResponseStatus, err := h.TradeTransactionStatus(ctx, symbol, tradeResponse.ReturnData.Order)
+			tradeResponseStatus, err := apiH.TradeTransactionStatus(ctx, symbol, tradeResponse.ReturnData.Order)
 			if err != nil {
-				logger.ErrorContext(ctx, "Failed to process TradeTransactionStatus",
+				p.Logger.ErrorContext(ctx, "Failed to process TradeTransactionStatus",
 					logging.ErrorAttr(err),
 					logging.SymbolAttr(symbol))
 				continue
 			}
+			p.Logger.InfoContext(ctx, "Successfully processed TradeTransactionStatus", logging.RespAttr(tradeResponseStatus))
 
-			//reqStatus, err := tradeResponseStatus.CheckRequestStatus()
-			//if reqStatus == response.ACCEPTED || reqStatus == response.PENDING || reqStatus == response.REJECTED {
-			//	go sendPing(ctx, wsClient, logger, 5)
-			//}
-			logger.InfoContext(ctx, "Successfully processed TradeTransactionStatus", logging.RespAttr(tradeResponseStatus))
+			reqStatus, err := tradeResponseStatus.CheckRequestStatus()
+			order := model.Order{
+				Number:         tradeResponse.ReturnData.Order,
+				UserID:         1,
+				RequestStatus:  response.RequestStatusName[reqStatus],
+				Message:        tradeResponseStatus.ReturnData.Message,
+				TradeTransInfo: tradeTransInfo,
+			}
+			if reqStatus == response.REJECTED {
+				rsErr := &response.RequestStatusError{}
+				if errors.As(err, &rsErr) {
+					order = model.Order{
+						Number:         tradeResponse.ReturnData.Order,
+						UserID:         1,
+						RequestStatus:  rsErr.RequestStatus,
+						Message:        rsErr.Message,
+						TradeTransInfo: tradeTransInfo,
+					}
+				}
+			}
+
+			ordID, err := dbH.InsertOrder(ctx, order)
+			if err != nil {
+				p.Logger.ErrorContext(ctx, "Failed to Insert Order",
+					logging.ErrorAttr(err),
+					logging.SymbolAttr(symbol))
+				continue
+			}
+			p.Logger.InfoContext(ctx, "Inserted order", logging.IDAttr(ordID))
+
+			position := model.Position{
+				UserID:              1,
+				OrderID:             ordID,
+				PurchaseInstruction: purchaseInstruction,
+			}
+			posID, err := dbH.InsertPosition(ctx, position)
+			if err != nil {
+				p.Logger.ErrorContext(ctx, "Failed to Insert Position",
+					logging.ErrorAttr(err),
+					logging.SymbolAttr(symbol))
+				continue
+			}
+			p.Logger.InfoContext(ctx, "Inserted position", logging.IDAttr(posID))
 		}
 
 		// logout user after processing
-		logoutResponse, err := h.Logout(ctx)
+		logoutResponse, err := apiH.Logout(ctx)
 		if err != nil {
-			logger.WarnContext(ctx, "Failed to process Logout - killing client", logging.ErrorAttr(err))
+			p.Logger.WarnContext(ctx, "Failed to process Logout - killing client", logging.ErrorAttr(err))
 			return
 		}
-		logger.InfoContext(ctx, "Successfully processed Logout", logging.RespAttr(logoutResponse))
+		p.Logger.InfoContext(ctx, "Successfully processed Logout", logging.RespAttr(logoutResponse))
 
-		logger.Info("Purchases request fully processed")
+		p.Logger.Info("Purchases request fully processed")
 	}
 }
 
-func PurchaseHandler(cfg config.AppConfig, wsManager *websocket.WSManager, logger *slog.Logger) http.HandlerFunc {
+func (p *Purchase) PurchaseHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
-		logger.InfoContext(ctx, "Start processing purchase request")
+		p.Logger.InfoContext(ctx, "Start processing purchase request")
 
 		connDetails, ok := ctx.Value(middleware.ConnDetailsKey).(middleware.ConnDetails)
 		if !ok {
-			logger.ErrorContext(ctx, "Failed to get ConnDetails")
+			p.Logger.ErrorContext(ctx, "Failed to get ConnDetails")
 			http.Error(w, "Could not retrieve client details", http.StatusInternalServerError)
 			return
 		}
 		traceID := connDetails.TraceID.String()
 
-		wsClient, err := wsManager.DialForNewClient(ctx, cfg.XTB.Demo.WebSocketURL, nil)
+		wsClient, err := p.WSManager.DialForNewClient(ctx, p.Cfg.XTB.Demo.WebSocketURL, nil)
 		if err != nil {
-			logger.ErrorContext(ctx, "Failed to create a new client", logging.ErrorAttr(err))
+			p.Logger.ErrorContext(ctx, "Failed to create a new client", logging.ErrorAttr(err))
 			http.Error(w, "Failed to establish client connection", http.StatusInternalServerError)
 			return
 		}
 		defer func() {
-			wsManager.RemoveClient(ctx, wsClient)
+			p.WSManager.RemoveClient(ctx, wsClient)
 		}()
 
-		h := Handler{
+		h := APIHandler{
 			Proc:    processor.NewProc(wsClient),
 			TraceID: traceID,
 		}
 
 		// log user into XTB
-		loginResponse, err := h.Login(ctx, cfg.XTB.Demo.UserID, cfg.XTB.Demo.Password)
+		loginResponse, err := h.Login(ctx, p.Cfg.XTB.Demo.UserID, p.Cfg.XTB.Demo.Password)
 		if err != nil {
-			logger.ErrorContext(ctx, "Failed to login", logging.ErrorAttr(err))
+			p.Logger.ErrorContext(ctx, "Failed to login", logging.ErrorAttr(err))
 			http.Error(w, "Failed to login", http.StatusBadRequest)
 			return
 		}
-		logger.InfoContext(ctx, "Successfully processed Login", logging.RespAttr(loginResponse))
+		p.Logger.InfoContext(ctx, "Successfully processed Login", logging.RespAttr(loginResponse))
 
 		// get purchase instructions
 		var purchaseInstruction model.PurchaseInstruction
 		err = json.NewDecoder(r.Body).Decode(&purchaseInstruction)
 		if err != nil {
-			logger.ErrorContext(ctx, "Failed to read request body with purchase instruction", logging.ErrorAttr(err))
+			p.Logger.ErrorContext(ctx, "Failed to read request body with purchase instruction", logging.ErrorAttr(err))
 			http.Error(w, "Failed to read body", http.StatusBadRequest)
 			return
 		}
 
-		logger.InfoContext(ctx, "Will process purchase instruction", logging.PurchaseInstrAttr(purchaseInstruction))
+		p.Logger.InfoContext(ctx, "Will process purchase instruction", logging.PurchaseInstrAttr(purchaseInstruction))
 
 		symbol := purchaseInstruction.Symbol
 
 		// get symbol details
 		symbolResponse, err := h.GetSymbolExtended(ctx, symbol)
 		if err != nil {
-			logger.ErrorContext(ctx, "Failed to process GetSymbol",
+			p.Logger.ErrorContext(ctx, "Failed to process GetSymbol",
 				logging.ErrorAttr(err),
 				logging.SymbolAttr(symbol))
 			http.Error(w, "Failed to execute GetSymbol", http.StatusInternalServerError)
 			return
 		}
-		logger.InfoContext(ctx, "Successfully processed GetSymbol", logging.RespAttr(symbolResponse))
+		p.Logger.InfoContext(ctx, "Successfully processed GetSymbol", logging.RespAttr(symbolResponse))
 
 		// prepare TradeTransactionInfo to pass it to TradeTransaction as argument
 		tradeTransInfo, err := purchaseInstruction.PrepareTradeTransInfo(symbolResponse, 0.01)
 		if err != nil {
-			logger.WarnContext(ctx, "Failed to prepare TradeTransInfo",
+			p.Logger.WarnContext(ctx, "Failed to prepare TradeTransInfo",
 				logging.ErrorAttr(err),
 				logging.SymbolAttr(symbol))
 			http.Error(w, fmt.Sprintf("Failed to prepare TradeTransInfo: %v", err), http.StatusBadRequest)
@@ -194,18 +252,18 @@ func PurchaseHandler(cfg config.AppConfig, wsManager *websocket.WSManager, logge
 		// process TradeTransaction
 		tradeResponse, err := h.TradeTransaction(ctx, symbol, tradeTransInfo)
 		if err != nil {
-			logger.ErrorContext(ctx, "Failed to process TradeTransaction",
+			p.Logger.ErrorContext(ctx, "Failed to process TradeTransaction",
 				logging.ErrorAttr(err),
 				logging.SymbolAttr(symbol))
 			http.Error(w, "Failed to execute TradeTransaction", http.StatusInternalServerError)
 			return
 		}
-		logger.InfoContext(ctx, "Successfully processed TradeTransaction", logging.RespAttr(tradeResponse))
+		p.Logger.InfoContext(ctx, "Successfully processed TradeTransaction", logging.RespAttr(tradeResponse))
 
 		// process TradeTransactionStatus
 		tradeResponseStatus, err := h.TradeTransactionStatus(ctx, symbol, tradeResponse.ReturnData.Order)
 		if err != nil {
-			logger.ErrorContext(ctx, "Failed to process TradeTransactionStatus",
+			p.Logger.ErrorContext(ctx, "Failed to process TradeTransactionStatus",
 				logging.ErrorAttr(err),
 				logging.SymbolAttr(symbol))
 			http.Error(w, "Failed to execute TradeTransactionStatus", http.StatusInternalServerError)
@@ -214,19 +272,19 @@ func PurchaseHandler(cfg config.AppConfig, wsManager *websocket.WSManager, logge
 
 		//reqStatus, err := tradeResponseStatus.CheckRequestStatus()
 		//if reqStatus == response.ACCEPTED || reqStatus == response.PENDING || reqStatus == response.REJECTED {
-		//	go sendPing(ctx, wsClient, logger, 5)
+		//	go sendPing(ctx, wsClient, p.Logger, 5)
 		//}
-		logger.InfoContext(ctx, "Successfully processed TradeTransactionStatus", logging.RespAttr(tradeResponseStatus))
+		p.Logger.InfoContext(ctx, "Successfully processed TradeTransactionStatus", logging.RespAttr(tradeResponseStatus))
 
 		// logout user after processing
 		logoutResponse, err := h.Logout(ctx)
 		if err != nil {
-			logger.WarnContext(ctx, "Failed to process Logout - killing client", logging.ErrorAttr(err))
+			p.Logger.WarnContext(ctx, "Failed to process Logout - killing client", logging.ErrorAttr(err))
 			http.Error(w, "Failed to execute TradeTransactionStatus", http.StatusInternalServerError)
 			return
 		}
-		logger.InfoContext(ctx, "Successfully processed Logout", logging.RespAttr(logoutResponse))
+		p.Logger.InfoContext(ctx, "Successfully processed Logout", logging.RespAttr(logoutResponse))
 
-		logger.Info("Purchase request fully processed")
+		p.Logger.Info("Purchase request fully processed")
 	}
 }
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/artufi/trader/infrastructure/database"
 	"github.com/artufi/trader/infrastructure/websocket"
 	"github.com/artufi/trader/logging"
+	"github.com/artufi/trader/model"
 	"github.com/go-chi/chi/v5"
 	ws "github.com/gorilla/websocket"
 	"log/slog"
@@ -26,6 +27,7 @@ func main() {
 	defer db.Close()
 	err := database.Connect(db, 10, time.Second*2)
 	if err != nil {
+		logger.Info("Failed to connect with database", logging.ErrorAttr(err))
 		panic(err)
 	}
 	logger.Info("Connected to database", logging.URLAttr(cfg.Database.Host+cfg.Database.Port))
@@ -33,10 +35,19 @@ func main() {
 	dialer := &ws.Dialer{}
 	wsManager := websocket.NewWSManager(dialer, logger)
 
+	purchaseC := controller.Purchase{
+		Cfg:             cfg,
+		WSManager:       wsManager,
+		Logger:          logger,
+		PositionService: model.PositionService{DB: db},
+		OrderService:    model.OrderService{DB: db},
+	}
+
 	r := chi.NewRouter()
 	r.Use(middleware.ConnDetailsMiddleware(cfg, logger))
-	r.Get("/purchases", controller.PurchasesHandler(cfg, wsManager, logger))
-	r.Get("/purchase", controller.PurchaseHandler(cfg, wsManager, logger))
+	r.Get("/purchases", purchaseC.PurchasesHandler())
+	r.Get("/purchase", purchaseC.PurchaseHandler())
+
 	r.Get("/purchase/status", controller.TransactionStatusHandler(cfg, wsManager, logger))
 
 	logger.Info("Starting application port: 4000...")
