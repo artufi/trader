352291f6b81525065771ae6be2bf5bdc7ffa1cf8
artufi <aartur.fil@gmail.com>
2024-04-02 21:25:53 +0200

    Update logging attributes

--- logging/attributes.go
+++ logging/attributes.go
@@ -6,7 +6,7 @@ import (
 )
 
 func URLAttr(url string) slog.Attr {
-	return slog.Any("URL", url)
+	return slog.String("URL", url)
 }
 
 func ErrorAttr(err error) slog.Attr {
@@ -14,23 +14,23 @@ func ErrorAttr(err error) slog.Attr {
 }
 
 func UserIDAttr(userID string) slog.Attr {
-	return slog.Any("userID", userID)
+	return slog.String("userID", userID)
 }
 
-func ClientID(clientID string) slog.Attr {
-	return slog.Any("clientID", clientID)
+func ConnNo(connNumber int) slog.Attr {
+	return slog.Int("connNo", connNumber)
 }
 
 func TraceIDAttr(traceID string) slog.Attr {
-	return slog.Any("traceID", traceID)
+	return slog.String("traceID", traceID)
 }
 
 func MsgAttr(msg string) slog.Attr {
-	return slog.Any("msg", msg)
+	return slog.String("msg", msg)
 }
 
 func SymbolAttr(symbol string) slog.Attr {
-	return slog.Any("symbol", symbol)
+	return slog.String("symbol", symbol)
 }
 
 func PredictionDetailsAttr(predictionDetails model.PredictionDetails) slog.Attr {
@@ -42,5 +42,9 @@ func RespAttr(resp interface{}) slog.Attr {
 }
 
 func IDAttr(id int) slog.Attr {
-	return slog.Any("id", id)
+	return slog.Int("id", id)
+}
+
+func StreamID(ssid string) slog.Attr {
+	return slog.String("streamID", ssid)
 }
