c0c25e78138a6e760e867214a6acc6e5e3ec9f62
artufi <aartur.fil@gmail.com>
2024-04-26 10:35:38 +0200

    Dial for new streaming client in manager

--- infrastructure/websocket/manager.go
+++ infrastructure/websocket/manager.go
@@ -64,6 +64,37 @@ func (wsm *WSManager) DialForNewClient(ctx context.Context, url string, requestH
 	return client, nil
 }
 
+func (wsm *WSManager) DialForNewStreamClient(ctx context.Context, url string, requestHeader http.Header, userID string) (*WSClientStream, error) {
+	conn, resp, err := wsm.dialer.Dial(url, requestHeader)
+	if err != nil {
+		if resp != nil {
+			resp.Body.Close()
+			wsm.logger.ErrorContext(ctx, "Manager failed to dial stream websocket", logging.ErrorAttr(err), logging.URLAttr(url),
+				"statusCode", resp.StatusCode)
+		} else {
+			wsm.logger.ErrorContext(ctx, "Manager failed to dial stream websocket", logging.ErrorAttr(err), logging.URLAttr(url))
+		}
+		return nil, fmt.Errorf("manager failed to establish stream websocket connection: %w", err)
+	}
+
+	randomClient, err := wsm.GetUserRandClientWithFreeSSID(userID)
+	if err != nil {
+		return nil, fmt.Errorf("manager failed to get client for stream connection: %w", err)
+	}
+	//randomClient.Stream = true
+	client := &WSClientStream{
+		conn:            conn,
+		manager:         wsm,
+		logger:          wsm.logger,
+		sendRateLimiter: time.NewTicker(200 * time.Millisecond),
+		UserID:          userID,
+		StreamSessionID: randomClient.StreamSessionID,
+		client:          randomClient,
+	}
+
+	return client, nil
+}
+
 func (wsm *WSManager) addClient(ctx context.Context, client *WSClient) {
 	wsm.Lock()
 	defer wsm.Unlock()
@@ -101,5 +132,16 @@ func (wsm *WSManager) GetUserRandomClient(userID string) (*WSClient, error) {
 			return clientList[rand.Intn(len(clientList))], nil
 		}
 	}
-	return nil, fmt.Errorf("manager: no clients for user: %v", userID)
+	return nil, fmt.Errorf("manager no clients for user: %v", userID)
+}
+
+func (wsm *WSManager) GetUserRandClientWithFreeSSID(userID string) (*WSClient, error) {
+	if clients, ok := wsm.userClients[userID]; ok {
+		for client := range clients {
+			if !client.Stream {
+				return client, nil
+			}
+		}
+	}
+	return nil, fmt.Errorf("manager no free streaming clients for user: %v", userID)
 }
