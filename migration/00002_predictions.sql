b59818f252c72504c8d28282972674fb7a371b9a
artufi <aartur.fil@gmail.com>
2024-05-19 10:14:11 +0200

    Add goose migration files

---
+++ migration/00002_predictions.sql
@@ -0,0 +1,27 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE predictions
+(
+    id                  SERIAL PRIMARY KEY,
+    user_id             INT,
+    symbol              TEXT,
+    prediction_date     TEXT,
+    allocation          DOUBLE PRECISION,
+    preds_proba         DOUBLE PRECISION,
+    stop_loss           DOUBLE PRECISION,
+    take_profit         DOUBLE PRECISION,
+    forecast_horizon    INT,
+    target_change       INT,
+    model_type          TEXT,
+    run_timestamp       TEXT,
+    sl_tp_logic         TEXT,
+    id_model_properties TEXT,
+    created_at          TIMESTAMPTZ,
+    CONSTRAINT fk_predictions_user FOREIGN KEY (user_id) REFERENCES users (id)
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP table predictions;
+-- +goose StatementEnd
