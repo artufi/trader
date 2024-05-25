77b08c6b996db5d4dfc1d6ab219f864b6b58a5dc
artufi <aartur.fil@gmail.com>
2024-05-25 16:20:19 +0200

    Add new methods to the XTB API method set to process trade history

--- xtb/command/trade.go
+++ xtb/command/trade.go
@@ -12,3 +12,20 @@ type GetTradesStream struct {
 	Command         string `json:"command"`
 	StreamSessionId string `json:"streamSessionId"`
 }
+
+type GetTradesHistory struct {
+	Command   string `json:"command"`
+	Arguments struct {
+		End   int `json:"end"`
+		Start int `json:"start"`
+	} `json:"arguments"`
+	CustomTag string `json:"customTag"`
+}
+
+type GetTradeRecords struct {
+	Command   string `json:"command"`
+	Arguments struct {
+		Orders []int `json:"orders"`
+	} `json:"arguments"`
+	CustomTag string `json:"customTag"`
+}
