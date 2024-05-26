9f065ce9f48b768f7333362bf00ed13fce57d715
artufi <aartur.fil@gmail.com>
2024-05-26 09:58:46 +0200

    Select open orders from database, include a new field in orders table

--- model/order.go
+++ model/order.go
@@ -12,7 +12,9 @@ type Order struct {
 	ID int
 	// xtb order no
 	Number int
-	UserID int
+	// xtb position, null indicates position get failure
+	Position *int
+	UserID   int
 	// from PredictionDetails
 	PredictionID int
 
@@ -29,10 +31,81 @@ type OrderService struct {
 	DB *sql.DB
 }
 
+func (os OrderService) SelectOpenOrdersWithPosition() ([]Order, error) {
+	// now() returns UTC timezone, created_at has timezone from application
+	// and need to be converted into UTC
+	rows, err := os.DB.Query(`
+				SELECT 
+				    o.order_no,
+				    o.position,
+				    o.symbol,
+				    o.volume,
+				    o.closed
+				FROM orders o
+				JOIN predictions p ON o.prediction_id = p.id
+                WHERE 
+                	o.position IS NOT NULL 
+                  	AND o.closed = false
+                  	AND (now() - p.interval::INTERVAL) > (o.created_at AT TIME ZONE 'Universal')`)
+	if err != nil {
+		return nil, fmt.Errorf("select open positions: %w", err)
+	}
+	defer rows.Close()
+
+	orders := make([]Order, 0, 0)
+	for rows.Next() {
+		order := Order{}
+		err = rows.Scan(&order.Number, &order.Position, &order.Symbol, &order.Volume, &order.Closed)
+		if err != nil {
+			return nil, fmt.Errorf("select open positions: %w", err)
+		}
+		orders = append(orders, order)
+	}
+	//err = rows.Err()
+
+	return orders, nil
+}
+
+// get position to update order record?
+func (os OrderService) SelectOpenOrders() ([]Order, error) {
+	rows, err := os.DB.Query(`
+				SELECT 
+				    o.order_no,
+				    o.position,
+				    o.symbol,
+				    o.volume,
+				    o.closed
+				FROM orders o
+				JOIN predictions p ON o.prediction_id = p.id
+                WHERE
+                  	o.closed = false
+                  	AND (now() - p.interval::INTERVAL) > (o.created_at AT TIME ZONE 'Universal')`)
+	if err != nil {
+		return nil, fmt.Errorf("select open orders: %w", err)
+	}
+	defer rows.Close()
+
+	orders := make([]Order, 0, 0)
+	for rows.Next() {
+		order := Order{}
+		err = rows.Scan(&order.Number, &order.Position, &order.Symbol, &order.Volume, &order.Closed)
+		if err != nil {
+			return nil, fmt.Errorf("select open orders: %w", err)
+		}
+		if order.Number != 0 {
+			orders = append(orders, order)
+		}
+	}
+	//err = rows.Err()
+
+	return orders, nil
+}
+
 func (os OrderService) Insert(o Order) (int, error) {
 	row := os.DB.QueryRow(`
 		INSERT INTO orders
 			(order_no,
+			 position,
 			 user_id,
 			 prediction_id,
 			 symbol,
@@ -57,8 +130,8 @@ func (os OrderService) Insert(o Order) (int, error) {
 			 close_time,
 			 created_at)
 		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, 
-		        $23, $24)
-		RETURNING id`, o.Number, o.UserID, o.PredictionID, o.Symbol, o.Cmd, o.Type, o.CustomComment, o.Expiration,
+		        $23, $24, $25)
+		RETURNING id`, o.Number, o.Position, o.UserID, o.PredictionID, o.Symbol, o.Cmd, o.Type, o.CustomComment, o.Expiration,
 		o.Offset, o.Order, o.Price, o.Sl, o.Tp, o.Volume, o.RequestStatus, o.Message, o.OpenPrice, o.ClosePrice,
 		o.Profit, o.Closed, o.Comment, o.OpenTime, o.CloseTime, time.Now())
 
