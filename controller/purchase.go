48aade8ba02c950cad4f2e4a4e905de440947c75
artufi <aartur.fil@gmail.com>
2024-05-26 10:06:21 +0200

    Try to get order position after the tradeTransaction when processing request in purchase controller

--- controller/purchase.go
+++ controller/purchase.go
@@ -86,24 +86,24 @@ func (p *Purchase) PurchasesHandler() http.HandlerFunc {
 			}
 
 			// process TradeTransaction
-			tradeResponse, err := apiH.TradeTransaction(ctx, symbol, tradeTransInfo)
+			tradeTransResp, err := apiH.TradeTransaction(ctx, symbol, tradeTransInfo)
 			if err != nil {
 				p.Logger.ErrorContext(ctx, "Failed to process TradeTransaction",
 					logging.ErrorAttr(err),
 					logging.SymbolAttr(symbol))
 				continue
 			}
-			p.Logger.InfoContext(ctx, "Successfully processed TradeTransaction", logging.RespAttr(tradeResponse))
+			p.Logger.InfoContext(ctx, "Successfully processed TradeTransaction", logging.RespAttr(tradeTransResp))
 
 			// process TradeTransactionStatus
-			tradeResponseStatus, err := apiH.TradeTransactionStatus(ctx, symbol, tradeResponse.ReturnData.Order)
+			tradeTransStatusResp, err := apiH.TradeTransactionStatus(ctx, symbol, tradeTransResp.ReturnData.Order)
 			if err != nil {
 				p.Logger.ErrorContext(ctx, "Failed to process TradeTransactionStatus",
 					logging.ErrorAttr(err),
 					logging.SymbolAttr(symbol))
 				continue
 			}
-			p.Logger.InfoContext(ctx, "Successfully processed TradeTransactionStatus", logging.RespAttr(tradeResponseStatus))
+			p.Logger.InfoContext(ctx, "Successfully processed TradeTransactionStatus", logging.RespAttr(tradeTransStatusResp))
 
 			prediction := model.Prediction{
 				UserID:            1,
@@ -118,20 +118,41 @@ func (p *Purchase) PurchasesHandler() http.HandlerFunc {
 			}
 			p.Logger.InfoContext(ctx, "Inserted prediction", logging.IDAttr(predID))
 
-			reqStatus, err := tradeResponseStatus.CheckRequestStatus()
-			order := model.Order{
-				Number:         tradeResponse.ReturnData.Order,
-				UserID:         1,
-				PredictionID:   predID,
-				RequestStatus:  response.RequestStatusName[reqStatus],
-				Message:        tradeResponseStatus.ReturnData.Message,
-				TradeTransInfo: tradeTransInfo,
-			}
-			if reqStatus == response.REJECTED {
+			var order model.Order
+			reqStatus, errReq := tradeTransStatusResp.CheckRequestStatus()
+			if reqStatus != response.REJECTED {
+				// getTrades to acquire position required to forcibly close purchase on demand in history service
+				getTradesResp, err := apiH.GetTrades(ctx, symbol, true)
+				if err != nil {
+					p.Logger.WarnContext(ctx, "Failed to process GetTrades, position number will not be added",
+						logging.ErrorAttr(err),
+						logging.SymbolAttr(symbol))
+				}
+				p.Logger.InfoContext(ctx, "Successfully processed GetTrades", logging.RespAttr(getTradesResp))
+
+				var position int
+				for _, tradeRecord := range getTradesResp.ReturnData {
+					if tradeTransResp.ReturnData.Order == tradeRecord.Order2 {
+						position = tradeRecord.Position
+						p.Logger.InfoContext(ctx, "Acquired order position", logging.PositionAttr(position))
+						break
+					}
+				}
+
+				order = model.Order{
+					Number:         tradeTransResp.ReturnData.Order,
+					UserID:         1,
+					Position:       &position,
+					PredictionID:   predID,
+					RequestStatus:  response.RequestStatusName[reqStatus],
+					Message:        tradeTransStatusResp.ReturnData.Message,
+					TradeTransInfo: tradeTransInfo,
+				}
+			} else {
 				rsErr := &response.RequestStatusError{}
-				if errors.As(err, &rsErr) {
+				if errors.As(errReq, &rsErr) {
 					order = model.Order{
-						Number:         tradeResponse.ReturnData.Order,
+						Number:         tradeTransResp.ReturnData.Order,
 						UserID:         1,
 						PredictionID:   predID,
 						RequestStatus:  rsErr.RequestStatus,
