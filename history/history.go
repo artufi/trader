5869462eb71e4bcad8decb7bb3e9fcc571aaff1c
artufi <aartur.fil@gmail.com>
2025-06-19 22:28:16 +0200

    Improve history.go code, add context propagation to enable proper cancellation

--- history/history.go
+++ history/history.go
@@ -16,7 +16,11 @@ import (
 	"time"
 )
 
-const serviceName = "HistoryService"
+const (
+	serviceName = "HistoryService"
+	// retryBackoff defines the wait duration after failed dialing for a new stream client.
+	retryBackoff = 10 * time.Second
+)
 
 type HService struct {
 	Cfg    config.AppConfig
@@ -29,43 +33,6 @@ type HService struct {
 	OrdersByPosition map[int]int
 
 	sync.Mutex
-}
-
-// GetTradesCurrentlyUnused - currently unused, perhaps will be modified to update not updated closed trades
-func (hs *HService) GetTradesCurrentlyUnused(ctx context.Context, userID string) {
-	wsClient, err := hs.WSManager.GetUserRandomClient(userID)
-	if err != nil {
-		hs.Logger.ErrorContext(ctx, "Failed to get client to process getTrades", logging.ErrorAttr(err))
-		return
-	}
-
-	// search for specific orders where closed is true, close_time is null i tts_message jest puste lub null?
-
-	proc := processor.NewProc(wsClient)
-	getTradesResp, err := proc.GetTrades(ctx, "getTrades"+wsClient.StreamSessionID, false)
-	if err != nil {
-		hs.Logger.ErrorContext(ctx, "Failed to process getTrades", logging.ErrorAttr(err))
-		return
-	}
-	hs.Logger.InfoContext(ctx, "Successfully processed getTrades", logging.RespAttr(getTradesResp))
-	for _, tradeRecord := range getTradesResp.ReturnData {
-		if tradeRecord.Closed {
-			updateDetails := model.OrderClosedDetails{
-				ClosePrice: tradeRecord.OpenPrice,
-				OpenPrice:  tradeRecord.ClosePrice,
-				Profit:     tradeRecord.Profit,
-				Closed:     tradeRecord.Closed,
-				Comment:    tradeRecord.Comment,
-				OpenTime:   nil,
-				CloseTime:  nil,
-			}
-			id, err := hs.OrderService.UpdateClosed(tradeRecord.Order2, updateDetails)
-			if err != nil {
-				hs.Logger.ErrorContext(ctx, "Failed to update closed order", logging.ErrorAttr(err))
-			}
-			hs.Logger.InfoContext(ctx, "Updated order", logging.IDAttr(id))
-		}
-	}
 }
 
 // GetTradesStream The streaming process of getTrades in XTB is complex in the context of the received response
@@ -97,91 +64,139 @@ func (hs *HService) GetTradesCurrentlyUnused(ctx context.Context, userID string)
 // in the last stage, and the position is considered Modified again, but now without being Deleted.
 // The Deleted state is likely a confirmation, but apparently deemed unnecessary here since
 // the position is already closed.
-func (hs *HService) GetTradesStream(userID string) {
-	ctx := logging.AppendAttrsCtx(context.Background(), logging.ServiceName(serviceName))
+func (hs *HService) GetTradesStream(ctx context.Context, userID string) {
+	ctx = logging.AppendAttrsCtx(ctx, logging.ServiceName(serviceName), logging.UserIDAttr(userID))
 	for {
+		if ctx.Err() != nil {
+			hs.Logger.InfoContext(ctx, "Context done, stop dialing for a new stream client to process getTrades",
+				logging.ErrorAttr(ctx.Err()))
+			return
+		}
 		wsClientStream, err := hs.WSManager.DialForNewStreamClient(ctx, hs.Cfg.XTB.Demo.WebSocketStreamURL, nil, userID)
 		if err != nil {
-			hs.Logger.ErrorContext(ctx, "Failed to get client to process getTradesStream, will try again...",
+			hs.Logger.ErrorContext(ctx, "Failed to dial for a new stream client to process getTrades will try again...",
 				logging.ErrorAttr(err))
-			time.Sleep(time.Second * 10)
+			// TODO: implement retry with a max attempt limit
+			time.Sleep(retryBackoff)
 			continue
 		}
 		ssid := wsClientStream.StreamSessionID
-		ctx := logging.AppendAttrsCtx(ctx, logging.StreamID(ssid))
+		ctx = logging.AppendAttrsCtx(ctx, logging.StreamID(ssid))
 
 		proc := processor.NewProc(wsClientStream)
 		_, err = proc.GetTradesStream(ctx, ssid)
 		if err != nil {
-			hs.Logger.ErrorContext(ctx, "Failed to process getTradesStream", logging.ErrorAttr(err))
+			hs.Logger.ErrorContext(ctx, "Failed to start getTrades stream", logging.ErrorAttr(err))
 			continue
 		}
 		for {
-			resp := <-wsClientStream.ReaderRespCh
-			if resp.Err != nil {
-				hs.Logger.ErrorContext(ctx, "Failed read getTradesStream", logging.ErrorAttr(resp.Err))
-				break
-			}
-
-			tradeResponseStream := response.GetTradeStream{}
-			err = json.Unmarshal(resp.Data, &tradeResponseStream)
-			if err != nil {
-				hs.Logger.ErrorContext(ctx, "Failed to deserialize getTradesStream", logging.ErrorAttr(err))
-				continue
-			}
-			hs.Logger.InfoContext(ctx, "getTradesStream response", logging.RespAttr(tradeResponseStream))
-
-			// Currently, I haven't found a better way to distinguish unnecessary transactions
-			// from those I'm looking for because the positionID and orderID are different in
-			// the case of closing positions. When closing a position, the last message containing
-			// the most important details has a position number assigned earlier but a different
-			// orderID (usually, closing positions consist of 3 messages: the first has an orderID
-			// consistent with the database, the next one has an orderID consistent with the database,
-			// and the final one has an orderID incremented by 2). Even though the details of this
-			// operation are related to the sought orderID, it's necessary to save the position as
-			// a key in the map (because it's always the same), and to save the orderID as a value,
-			// but remember that in the final step, you shouldn't save the orderID because it will
-			// be a different value than the orderID saved in the database during position opening.
-			// It's also important to remove these elements from the map to prevent it from overflowing.
-			//
-			// Unfortunately, in this case, a record with a position number the same as the orderID,
-			// indicating the opening of the position, is also stored in the map, so this unnecessary
-			// (for now) entry must be removed from the map to prevent overflow. At the moment,
-			// I haven't found any indicator that specifies that this is an opening position transaction.
-			// I've only managed to exclude operations like PENDING, which don't contribute anything
-			// to the system and aren't added to the map.
-			tradeData := tradeResponseStream.Data
-			position := tradeData.Position
-			if !tradeData.Closed && tradeData.Type != int(command.PENDING) {
-				hs.Lock()
-				hs.OrdersByPosition[position] = tradeData.Order2
-				hs.Unlock()
-			} else if tradeData.Closed && tradeData.Type == int(command.CLOSE) {
-				openTime := time.UnixMilli(int64(*tradeData.OpenTime))
-				closeTime := time.UnixMilli(int64(*tradeData.CloseTime))
-				updateDetails := model.OrderClosedDetails{
-					ClosePrice: tradeData.ClosePrice,
-					OpenPrice:  tradeData.OpenPrice,
-					Profit:     tradeData.Profit,
-					Closed:     tradeData.Closed,
-					Comment:    tradeData.Comment,
-					OpenTime:   &openTime,
-					CloseTime:  &closeTime,
+			select {
+			case <-ctx.Done():
+				hs.Logger.InfoContext(ctx, "Context done, stop getTrades stream read loop")
+				return
+			case resp := <-wsClientStream.ReaderRespCh:
+				if resp.Err != nil {
+					hs.Logger.ErrorContext(ctx, "Failed to read getTrades stream", logging.ErrorAttr(resp.Err))
+					break
 				}
 
-				hs.Lock()
-				orderID := hs.OrdersByPosition[position]
-				dbID, err := hs.OrderService.UpdateClosed(orderID, updateDetails)
+				tradeResponseStream := response.GetTradeStream{}
+				err = json.Unmarshal(resp.Data, &tradeResponseStream)
 				if err != nil {
-					hs.Logger.ErrorContext(ctx, "getTradesStream failed to update closed order", logging.ErrorAttr(err))
-				} else {
-					hs.Logger.InfoContext(ctx, "getTradesStream updated order stream", logging.IDAttr(dbID))
+					hs.Logger.ErrorContext(ctx, "Failed to deserialize getTrades stream data", logging.ErrorAttr(err))
+					continue
+				}
+				hs.Logger.InfoContext(ctx, "getTrades stream response", logging.RespAttr(tradeResponseStream))
+
+				// Currently, I haven't found a better way to distinguish unnecessary transactions
+				// from those I'm looking for because the positionID and orderID are different in
+				// the case of closing positions. When closing a position, the last message containing
+				// the most important details has a position number assigned earlier but a different
+				// orderID (usually, closing positions consist of 3 messages: the first has an orderID
+				// consistent with the database, the next one has an orderID consistent with the database,
+				// and the final one has an orderID incremented by 2). Even though the details of this
+				// operation are related to the sought orderID, it's necessary to save the position as
+				// a key in the map (because it's always the same), and to save the orderID as a value,
+				// but remember that in the final step, you shouldn't save the orderID because it will
+				// be a different value than the orderID saved in the database during position opening.
+				// It's also important to remove these elements from the map to prevent it from overflowing.
+				//
+				// Unfortunately, in this case, a record with a position number the same as the orderID,
+				// indicating the opening of the position, is also stored in the map, so this unnecessary
+				// (for now) entry must be removed from the map to prevent overflow. At the moment,
+				// I haven't found any indicator that specifies that this is an opening position transaction.
+				// I've only managed to exclude operations like PENDING, which don't contribute anything
+				// to the system and aren't added to the map.
+				tradeData := tradeResponseStream.Data
+				position := tradeData.Position
+				if !tradeData.Closed && tradeData.Type != int(command.PENDING) {
+					hs.Lock()
+					hs.OrdersByPosition[position] = tradeData.Order2
+					hs.Unlock()
+				} else if tradeData.Closed && tradeData.Type == int(command.CLOSE) {
+					openTime := time.UnixMilli(int64(*tradeData.OpenTime))
+					closeTime := time.UnixMilli(int64(*tradeData.CloseTime))
+					updateDetails := model.OrderClosedDetails{
+						ClosePrice: tradeData.ClosePrice,
+						OpenPrice:  tradeData.OpenPrice,
+						Profit:     tradeData.Profit,
+						Closed:     tradeData.Closed,
+						Comment:    tradeData.Comment,
+						OpenTime:   &openTime,
+						CloseTime:  &closeTime,
+					}
+
+					hs.Lock()
+					orderID := hs.OrdersByPosition[position]
+					dbID, err := hs.OrderService.UpdateClosed(orderID, updateDetails)
+					if err != nil {
+						hs.Logger.ErrorContext(ctx, "Failed to update closed order", logging.ErrorAttr(err))
+					} else {
+						hs.Logger.InfoContext(ctx, "Updated closed order", logging.IDAttr(dbID))
+					}
+
+					delete(hs.OrdersByPosition, orderID)
+					delete(hs.OrdersByPosition, position)
+					hs.Unlock()
 				}
 
-				delete(hs.OrdersByPosition, orderID)
-				delete(hs.OrdersByPosition, position)
-				hs.Unlock()
 			}
+		}
+	}
+}
+
+// GetTradesCurrentlyUnused - currently unused, perhaps will be modified to update not updated closed trades.
+func (hs *HService) GetTradesCurrentlyUnused(ctx context.Context, userID string) {
+	wsClient, err := hs.WSManager.GetUserRandomClient(userID)
+	if err != nil {
+		hs.Logger.ErrorContext(ctx, "Failed to get client to process getTrades", logging.ErrorAttr(err))
+		return
+	}
+
+	// Search for specific orders where closed is true.
+	proc := processor.NewProc(wsClient)
+	getTradesResp, err := proc.GetTrades(ctx, "getTrades"+wsClient.StreamSessionID, false)
+	if err != nil {
+		hs.Logger.ErrorContext(ctx, "Failed to process getTrades", logging.ErrorAttr(err))
+		return
+	}
+	hs.Logger.InfoContext(ctx, "Successfully processed getTrades", logging.RespAttr(getTradesResp))
+	for _, tradeRecord := range getTradesResp.ReturnData {
+		if tradeRecord.Closed {
+			updateDetails := model.OrderClosedDetails{
+				ClosePrice: tradeRecord.OpenPrice,
+				OpenPrice:  tradeRecord.ClosePrice,
+				Profit:     tradeRecord.Profit,
+				Closed:     tradeRecord.Closed,
+				Comment:    tradeRecord.Comment,
+				OpenTime:   nil,
+				CloseTime:  nil,
+			}
+			id, err := hs.OrderService.UpdateClosed(tradeRecord.Order2, updateDetails)
+			if err != nil {
+				hs.Logger.ErrorContext(ctx, "Failed to update closed order", logging.ErrorAttr(err))
+			}
+			hs.Logger.InfoContext(ctx, "Updated order", logging.IDAttr(id))
 		}
 	}
 }
